a1ea8b830b2310032b14ec559807046eca96409a
thejherb <jbherb17@gmail.com>
2025-01-20 20:43:36 -0700

    changes

--- tire_volume.py
+++ tire_volume.py
@@ -1,4 +1,5 @@
 import math
+from datetime import datetime
 pi = math.pi
 w = float(input("Enter the width of the tire in mm (ex 205): "))
 a = float(input("Enter the aspect ratio of the tire (ex 60): "))
@@ -7,3 +8,8 @@ d = float(input("Enter the diameter of the wheel in inches (ex 15): "))
 v = (pi*(w*w)*a*(w*a+2540*d))/10000000000
 vround = round(v,2)
 print(f"The approximate volume is {vround} liters")
+
+today = datetime.now().date()
+ 
+with open("volumes.txt", "at") as volumes_file:
+    volumes_file.write(f"{today}, {w}, {a}, {d}, {vround}\n")
